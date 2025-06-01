9daa9b3408c42891fde3535d6a672d2c834b0b54
Karina Ryabinkina <karina.ryabinkina@B-J6F90C7Q-0727.local>
2025-06-01 21:39:58 +0300

    New memes

--- js.js
+++ js.js
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", function () {
   // Список изображений
   
- const images = Array.from({ length: 70 }, (_, index) => `mood/mood${index + 1}.jpg`);
+ const images = Array.from({ length: 89 }, (_, index) => `mood/mood${index + 1}.jpg`);
 console.log(images);
 
 
